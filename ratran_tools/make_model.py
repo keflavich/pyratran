552a5000d319847f387848756118401125e2229d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-12-05 18:38:36 -0700

    add a W51 model and start using real units in some places

--- ratran_tools/make_model.py
+++ ratran_tools/make_model.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 import numpy as np
 from numpy import pi
-import pyfits
+from astropy.io import fits
+from astropy import units as u
+from astropy import constants
 import os
 
 pc = 3.086e16 # m  (RATRAN uses meters)
@@ -10,6 +12,13 @@ msun = 2e33 # grams
 mh = 1.67e-24
 G = 6.67e-8 # CGS
 
+RATRAN_PATH = os.getenv('RATRAN')
+if RATRAN_PATH:
+    AMC_PATH = os.path.join(RATRAN_PATH, 'bin/amc')
+    SKY_PATH = os.path.join(RATRAN_PATH, 'bin/sky')
+else:
+    AMC_PATH = "/usr/local/bin/amc"
+    SKY_PATH = "/usr/local/bin/sky"
 
 def make_model(filename, rmax, rproffunc, abund, tk, vr, td=None, dv_fwhm=1.0,
                ne=None, te=None, vz=None, ncells=20, gastodust=100.,
@@ -52,16 +61,31 @@ def make_model(filename, rmax, rproffunc, abund, tk, vr, td=None, dv_fwhm=1.0,
     vardict = {"tk":tk,"td":td,"vr":vr,"abund":abund,"ne":ne,"te":te,"vz":vz,"db":db,}
     for varname,var in vardict.items():
         if var is None:
-            # variable not specified
-            continue
+            if varname in ('vz','ne','te'):
+                continue
+            else:
+                # variable not specified: not good!
+                raise ValueError("Variable {0} not specified.".format(varname))
         elif np.isscalar(var) == 1:
             var = np.zeros(ncells)+var
+            vardict[varname] = var
             exec("%s = np.zeros(ncells) + %s" % (varname,varname))
+            locals()[varname] = var
+            print("{0} = {1} = {2}".format(varname,var,locals()[varname]))
         elif len(var) == ncells:
             # all is OK
             continue
         else:
             raise ValueError("Variable has wrong size.")
+
+    tk = vardict['tk']
+    td = vardict['td']
+    vr = vardict['vr']
+    abund = vardict['abund']
+    ne = vardict['ne']
+    te = vardict['te']
+    vz = vardict['vz']
+    db = vardict['db']
 
     for ii in range(ncells):
 
@@ -77,26 +101,56 @@ def make_model(filename, rmax, rproffunc, abund, tk, vr, td=None, dv_fwhm=1.0,
 
     outf.close()
 
-def mt03(r, column=1e22, krho=1.5, mass=1.0, rhoouter=1e3, zh2=2.8,mh=1.66053886e-24, r_inner=au):
+def mt03(radii, column=1e22*u.cm**-2, krho=1.5, mass=1.0*u.M_sun,
+         rhoouter=1e3*u.cm**-3, zh2=2.8, mh=1*u.Da, r_inner=1*u.au):
     """
     Remember, radius is given in METERS
     r_inner is given in cm
     """
-    r = r*1e2 # convert to cm
-    r[r<r_inner] = r_inner
-    r_outer = (mass*msun/(pi * column * mh * zh2))**0.5
-    A = (4-krho)*mass*msun / (4/3. * pi * r_outer**(4-krho))
-    # equivalent to prev. line A = mass*msun / (r_outer**(4-krho)) * (3*(4-krho)/(4.*pi))
 
-    rho = ((A * r**-krho / (zh2*mh)) * (r<r_outer) + rhoouter*zh2*mh *
-           (r>r_outer))
+    raise ValueError("mt03 doesn't really make sense in this context. "
+                     "You need to specify an external pressure, etc.")
+
+    radii = u.Quantity(radii, u.m)
+    r_inner = u.Quantity(r_inner, u.m)
+
+    column = u.Quantity(column, u.cm**-2)
+    mass = u.Quantity(mass, u.M_sun)
+
+    # handle inner singularity if non-scalar
+    try:
+        radii[radii<r_inner] = r_inner
+    except TypeError:
+        pass
+
+    #phi_rho = argH!!!
+
+    # eqn 12
+    phi_P = (3-kapparho)/3. * (radii / Rcl)**-kapparho
+
+    # gaseous fraction fg: pg 853
+    # (fg=0 means all stars, fg=1 means all gas)
+    fg = 1
+
+    # eqn 24
+    rho = (phi_rho**-1 * (3*phi_P/(3-kapparho))**(3/4.) *
+           fg**(1/4.) * (eps_core * mass / mstar)**0.5)
+
+
+    r_outer = ((mass/(pi * column * mh * zh2))**0.5).to(u.m)
+    AA = (4-krho)*mass / (4/3. * pi * (r_outer/r_inner)**(4-krho) * u.m**3)
+    # equivalent to prev. line
+    # A = mass*msun / (r_outer**(4-krho)) * (3*(4-krho)/(4.*pi))
+
+    rho = ((AA * (radii/r_inner)**-krho) * (radii<r_outer) +
+           (rhoouter*zh2*mh) * (radii>r_outer))
 
     nh2 = rho / zh2 / mh
 
-    return nh2
+    return nh2.to(u.cm**-3).value
 
 
-def plummer(r,mtot=0,a=0,zh2=2.8,mh=1.66053886e-24,ismeters=True):
+def plummer(r, mtot=0, a=0, zh2=2.8, mh=1.66053886e-24, ismeters=True):
     """
     Return the density given a Plummer profile
     """
@@ -144,7 +198,7 @@ def bonnorebert(r,ncenter=1e5,ximax=6.9,viso=0.24,zh2=2.8):
 
     return broken_powerlaw(r,rbreak=rbreak,n0=ncenter)
 
-def run_model(mdlname,amc="/usr/local/bin/amc",sky="/usr/local/bin/sky",**kwargs):
+def run_model(mdlname,amc=AMC_PATH,sky=SKY_PATH,**kwargs):
 
     make_amc(mdlname,**kwargs)
 
@@ -204,7 +258,7 @@ def make_averagespec(mdlname,trans="1,3,6",clobber=True):
 
     for tnum in trans.split(","):
         fn = mdlname+"_%03i.fits" % tnum
-        pf = pyfits.open(fn)
+        pf = fits.open(fn)
         #meanspec = pf[0].data.mean(axis=1).mean(axis=0)
         for k in "CTYPE1","CTYPE2","CDELT1","CDELT2","CRVAL1","CRVAL2","CRPIX1","CRPIX2":
             del pf[0].header[k]
@@ -218,10 +272,10 @@ def make_averagespec(mdlname,trans="1,3,6",clobber=True):
 
 if __name__ == "__main__":
 
-    doplummer_incloud=True
+    doplummer_incloud=False
     doplummer=False
-    dobp=True
-    doEvans=True
+    dobp=False
+    doEvans=False
     domt03=False
 
     if doplummer_incloud:
@@ -271,6 +325,55 @@ if __name__ == "__main__":
     if doEvans:
         make_model("l1544BE5.mdl",0.3*pc,bonnorebert,1e-9,20,0.0,viso=0.17,ncenter=1e6)
         run_model("l1544BE5",central="1000,2.73",pixsize=1.0,distance=140)
+
+    # w51 case
+
+    ncells = 100
+    make_model("pl_m200_kappa1.5.mdl",
+               rmax=10000*au,
+               rproffunc=broken_powerlaw,
+               abund=1e-10, # abundance
+               tk=np.linspace(600,100,ncells), # tk
+               vr=np.linspace(0,5.0,ncells), # vr
+               ncells=ncells,
+               rbreak=1000*au, # au
+               n0=1e8, # cm**-3
+               power=-1.5)
+    run_model("pl_m200_kappa1.5",
+              distance=5400, # pc
+              central="0.2,200", # arcsec, kelvin
+              pixsize=2.0,
+              molfile='a-ch3oh',
+              centralregion=5,
+              trans='12,55,56,178,197,251,254,378,437,462,463,513,519,523,564,568',
+             )
+
+    #from astropy import units as u
+    #print(mt03((5000*u.au).to(u.m).value,
+    #           column=1e24, # cm**-2
+    #           krho=1.5,
+    #           mass=200, # msun
+    #           rhoouter=1e3, # cm**-3
+    #           r_inner=100*au,
+    #          )
+    #     )
+    #make_model("mt03_rho1.5_mass200_col1e24.mdl",
+    #           10000*au,
+    #           mt03,
+    #           1e-9, # abundance
+    #           100, # tk
+    #           0.0, # vr
+    #           krho=1.5,
+    #           mass=200,
+    #           column=1e24)
+    #run_model("mt03_rho1.5_mass200_col1e24",
+    #          distance=5400, # pc
+    #          central="0.04,300", # arcsec, kelvin
+    #          pixsize=2.0,
+    #          molfile='a-ch3oh',
+    #          centralregion=5,
+    #          trans='12,55,56,178,197,251,254,378,437,462,463,513,519,523,564,568',
+    #         )
 
     # totally implausible
     #if domt03:
